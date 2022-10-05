96a318e38712e299a17ac13b2f229734ecd90926
Carlos Gutiérrez <carlosgutimo.3d@gmail.com>
2022-10-05 08:39:07 +0200

    Updated App Icon for iOS and Android
    + Some minor updates

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,10 +1,3 @@
-// Copyright (c) 2022, Very Good Ventures
-// https://verygood.ventures
-//
-// Use of this source code is governed by an MIT-style
-// license that can be found in the LICENSE file or at
-// https://opensource.org/licenses/MIT.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:matchfee/counter/counter.dart';
@@ -25,6 +18,8 @@ class App extends StatelessWidget {
       localizationsDelegates: const [
         AppLocalizations.delegate,
         GlobalMaterialLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+        DefaultWidgetsLocalizations.delegate,
       ],
       supportedLocales: AppLocalizations.supportedLocales,
       home: const CounterPage(),
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:matchfee/app/app.dart';
+import 'package:matchfee/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
